f2efbe86f6ddfdcc9d948ee95131389ef794000a
naqib2000 <muhammadnaqib2000@gmail.com>
2023-06-25 16:43:59 +0100

    Add button functionality

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 //show new quote
 function newQuote() {
+  // pick a random quote from apiQuote array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  //check if author field is blank if so then replace with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
 // GET quotes from API
@@ -19,6 +39,16 @@ async function getQuotes() {
     //catch error here
   }
 }
+
+// tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // on load
 getQuotes();
