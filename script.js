172c05580b16f627d6a1d696c5753d408e608fb8
naqib2000 <muhammadnaqib2000@gmail.com>
2023-06-25 15:25:29 +0100

    Add fetch quotes functionality

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let apiQuotes = [];
+
+//show new quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
 // GET quotes from API
 // use async fetch request with try catch statement
-// async can happen independently without affecting loading of browser
+// async can happen independently without affecting loading of page
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //catch error here
+  }
+}
+
+// on load
+getQuotes();
